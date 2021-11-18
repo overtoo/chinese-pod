8f3db7ed2c4858162d78170e233fd2e1e26d7a2f
overtoo <s_bajo@hotmail.com>
2021-11-18 09:49:45 +0800

    cleanup2

--- pages/index.js
+++ pages/index.js
@@ -21,16 +21,19 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Chinesepod series is split into 5 episode modules.</p>
+        <p>Chinesepod series split into 5 episode modules.</p>
       </section>{" "}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Intermediate</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, description, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
+              <br />
+              <small className={utilStyles.lightText}>{description}</small>
+
               <br />
             </li>
           ))}
